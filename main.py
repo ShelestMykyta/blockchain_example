eb2baa22c069ae0022d784dbc2d9b233c9bc6664
Mykyta Shelest <shelestmykyta2022@gmail.com>
2023-11-19 23:35:31 +0200

    feat: add create block and get chain endpoints

--- main.py
+++ main.py
@@ -24,13 +24,12 @@ class Blockchain:
     def get_previous_block(self):
         return self.chain[-1]
 
-
     def proof_of_work(self, previous_proof):
         new_proof = 1
         check_proof = False
 
         while check_proof is False:
-            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            hash_operation = hashlib.sha256(str(new_proof ** 2 - previous_proof ** 2).encode()).hexdigest()
             if hash_operation[:4] == '0000':
                 check_proof = True
             else:
@@ -51,9 +50,40 @@ class Blockchain:
                 return False
             previous_proof = previous_block['proof']
             proof = block['proof']
-            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+            hash_operation = hashlib.sha256(str(proof ** 2 - previous_proof ** 2).encode()).hexdigest()
             if hash_operation[:4] != '0000':
                 return False
             previous_block = block
             block_index += 1
         return True
+
+
+app = Flask(__name__)
+
+blockchain = Blockchain()
+
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {
+        'message': 'Congratulations, you just mined the block!',
+        'block': block
+    }
+    return jsonify(response), 200
+
+
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    response = {
+        'chain': blockchain.chain,
+        'len': len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+
+app.run(host='0.0.0.0', port=5001)
